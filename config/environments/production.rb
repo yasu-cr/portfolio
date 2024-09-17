9b981bc98aff038bc1172d6a2e874a4438dbd8e4
ya <150337096+yasu-cr@users.noreply.github.com>
2024-09-17 21:35:01 +0900

    Blocked host: localhostの解消

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "localhost"
   config.hosts << "portfolio-9ki9.onrender.com"
 end
