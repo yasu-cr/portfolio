6756d62c30475f7f9dbccf59fbce0a8ecfab9abe
ya <150337096+yasu-cr@users.noreply.github.com>
2024-09-17 21:39:54 +0900

    Blocked host: localhostの解消2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.hosts << "localhost"
   config.hosts << "portfolio-9ki9.onrender.com"
 end
