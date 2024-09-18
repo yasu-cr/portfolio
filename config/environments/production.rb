1ccb10ca1dec3f94c975f327c8b66e19a462b294
ya <150337096+yasu-cr@users.noreply.github.com>
2024-09-18 19:23:54 +0900

    エラーの修正3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #config.hosts << "localhost"
+  config.hosts << "localhost"
   config.hosts << "portfolio-9ki9.onrender.com" # Render のホスト名
   config.hosts << "dpg-crklqcm8ii6s738293e0-a.singapore-postgres.render.com"
   #config.hosts.clear # すべてのホストを許可する
