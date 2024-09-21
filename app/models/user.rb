8df0ad0dfd9ed2198cdb193293fad4582f241b58
ya <150337096+yasu-cr@users.noreply.github.com>
2024-09-22 00:44:25 +0900

    エラーメッセージの重複の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
-         validates :name, presence: true, length: { maximum: 255, message: '氏名は255文字以内で入力してください'}
+         validates :name, presence: true, length: { maximum: 255, message: '氏名は255文字以内で入力してください' }
          validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: :invalid_format }
-         validates :password, length: { minimum: 8 }, format: {
-                                      with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
-                                      message: '英数字8文字以上で入力してください'
-                                      }
+         validates :password, presence: true, length: { minimum: 8, message: 'パスワードは8文字以上でなければなりません。' }
+         validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+                                      message: '英数字8文字以上で入力してください。'}
 end
